1cccac9c60196c37b5c3a5d015992c36660bc45f
Sumedha Singh Rathor <sumedharathor@gmail.com>
2025-02-09 22:50:58 +0530

    Added Feature2

---
+++ frontend/src/components/Feature2/Feature2.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Feature2.css";
+
+function Feature2() {
+  return (
+    <div className="feature2 w-94vw flex flex-wrap items-center justify-between mx-[3em] p-[4em] border-1 border-black">
+      <p className="max-w-[675px] text-[3em] font-weight-semibold text-end">
+        Bookmark your notes for easy access later.
+      </p>
+      <div className="img2 w-[15em] h-[15em] shadow-[10px_10px_#131013] border-1 border-black"></div>
+    </div>
+  );
+}
+
+export default Feature2;
