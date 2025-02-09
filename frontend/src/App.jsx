d05479fdf211ce2e62588e5144409a2bb0f0782b
Yashi Garg <yashigarg016@gmail.com>
2025-02-09 23:02:33 +0530

    will be updated later

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,14 @@ import "./App.css";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
+import Feature1 from "./components/Feature1/Feature1";
 
 function App() {
   return (
     <div className="">
       <Header />
       <Hero />
+      <Feature1 />
       <Footer />
     </div>
   );
---
+++ frontend/src/components/Feature1/Feature1.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Feature1.css";
+
+function Feature1() {
+  return (
+    <div className="feature1 w-94vw flex flex-wrap items-center justify-between mx-[3em] p-[4em] border-1 border-black">
+      <p className="max-w-[675px] text-[3em] font-weight-semibold text-end">
+      Share your notes and get access to viewing others as well
+      </p>
+      <div className="img1 w-[15em] h-[15em] shadow-[10px_10px_#131013] border-1 border-black"></div>
+    </div>
+  );
+}
+
+export default Feature2;
