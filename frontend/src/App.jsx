dff54a2bdeca2fa51809058ee7748c7c807adf3f
Sumedha Singh Rathor <sumedharathor@gmail.com>
2025-02-11 01:51:31 +0530

    Navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,16 +5,18 @@ import Feature1 from "./components/Feature1/Feature1";
 import Feature2 from "./components/Feature2/Feature2";
 import Feature3 from "./components/Feature3/Feature3";
 import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="">
-      <Header />
+      <Navbar />
+      {/* <Header />
       <Hero />
       <Feature1 />
       <Feature2 />
       <Feature3 />
-      <Footer />
+      <Footer /> */}
     </div>
   );
 }
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,10 +2,16 @@ import React from "react";
 
 function Navbar() {
   return (
-    <div className="navbar flex items-center w-screen bg-black text-white font-medium px-[50px] py-[5px] gap-[10px]">
-      <div className="h-[50px] w-[50px] bg-white"></div>
-      NOTES <br />
-      KI DUKAAN
+    <div className="navbar flex items-center justify-between w-screen bg-black text-white font-medium px-[50px] py-[5px]">
+      <div className="flex items-center gap-[10px]">
+        <div className="h-[50px] w-[50px] bg-white rounded-[5px]"></div>
+        NOTES <br />
+        KI DUKAAN
+      </div>
+      <div className="text-black bg-dark-orange text-center font-bold w-[900px] rounded-[20px]">
+        SEARCH BAR
+      </div>
+      <div className="h-[50px] w-[50px] bg-white rounded-[10px]"></div>
     </div>
   );
 }
