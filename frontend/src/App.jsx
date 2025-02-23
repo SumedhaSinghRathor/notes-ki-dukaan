04939593c053ed7b5c5b4becbeb3e07d1f5e0817
Sumedha Singh Rathor <sumedharathor@gmail.com>
2025-02-23 18:54:18 +0530

    Update README.md

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,14 @@
 import "./App.css";
-import Header from "./components/Header";
-import Hero from "./components/Hero";
-import Feature1 from "./components/Feature1/Feature1";
+import Navbar from "./components/Navbar";
+import View from "./pages/View/View";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="w-screen">
-      <Header />
-      <Hero />
-      <Feature1 />
+      <Navbar />
+      <View />
+      <Footer />
     </div>
   );
 }
