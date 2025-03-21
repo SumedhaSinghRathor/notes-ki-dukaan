88eaa2e68e0ed734132afeea3e63d83855eacfc1
Sumedha Singh Rathor <sumedharathor@gmail.com>
2025-03-22 00:24:41 +0530

    Hover changes

--- frontend/src/pages/Home/Thumbnail.jsx
+++ frontend/src/pages/Home/Thumbnail.jsx
@@ -5,34 +5,35 @@ function Thumbnail() {
   const handleClick = () => console.log("Turn to solid bookmark");
 
   return (
-    <Link to="/view" className="w-3xs flex flex-col gap-1.5">
-      <div className="thumbnailimg w-full h-44 bg-light-orange border-1 border-black"></div>
+    <div className="w-3xs flex flex-col gap-1.5">
+      <Link className="thumbnailimg w-full h-44 bg-light-orange border-1 border-black"></Link>
       <div className="info flex flex-col gap-1">
         <div className="title_bookmark flex justify-between items-center">
-          <div className="title font-bold text-2xl">Basic Memory Unit</div>
+          <Link to="/view" className="title font-bold text-2xl hover:underline">
+            Basic Memory Unit
+          </Link>
           <i className="bx bx-bookmark text-2xl flex justify-center items-center text-dark-orange"></i>
         </div>
 
-        <div className="faculty text-base">Anil Kumar Swain</div>
-        <div className="tags_rating flex justify-between gap-2 items-center w-ful">
+        <div className="faculty text-base hover:underline">
+          Anil Kumar Swain
+        </div>
+        <div className="tags_rating flex justify-between items-center w-full">
           <div className="tags flex justify-between gap-2 whitespace-nowrap overflow-clip">
-            <div className="tag bg-light-orange py-1 px-2 rounded-lg border-1 border-black text-xs">
+            <div className="tag bg-light-orange hover:bg-dark-orange transition-colors py-1 px-2 rounded-lg border-1 border-black text-xs">
               COA
-            </div>
-            <div className="tag bg-light-orange py-1 px-2 rounded-lg border-1 border-black text-xs">
-              HPC
             </div>
           </div>
           <div className="rating flex justify-between gap-2 items-center">
-            <div className="num leading-1">4.27</div>
+            <div className="num">4.27</div>
             <i
-              className="bx bxs-star text-2xl text-yellow-300"
+              className="bx bxs-star text-2xl text-dark-orange"
               onClick={handleClick}
             ></i>
           </div>
         </div>
       </div>
-    </Link>
+    </div>
   );
 }
 
