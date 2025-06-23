e27a6cb2f93e1f35bee974412aab21236e633c37
Sumedha Singh Rathor <sumedharathor@gmail.com>
2025-06-23 22:30:28 +0530

    username and email

---
+++ backend/src/main/java/com/nkd/backend/model/User.java
@@ -0,0 +1,29 @@
+package com.nkd.backend.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "users")
+public class User {
+    @Id
+    private String id;
+    private String email;
+    private String name;
+    private String s;
+
+    // Getter and Setter for email
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    // Getter and Setter for name
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+}
