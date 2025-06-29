4af61492aa7701eaaed510bfd05799929b8a776c
Sumedha Singh Rathor <sumedharathor@gmail.com>
2025-06-29 15:35:27 +0530

    removing thumbnail list view

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,26 +1,15 @@
 import Filter from "../../components/Filter";
 import Thumbnail from "./Thumbnail";
-import ThumbnailList from "./ThumbnailList";
+import dummy_data from "../../assets/dummy_data.json";
 
 function Home() {
-  const list = false;
   return (
     <div>
       <Filter />
-      <div className="w-full h-fit px-6">
-        {list ? (
-          <div>
-            {Array.from({ length: 9 }).map((_, index) => (
-              <ThumbnailList />
-            ))}
-          </div>
-        ) : (
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 grid-flow-row auto-rows-min gap-12">
-            {Array.from({ length: 9 }).map((_) => (
-              <Thumbnail />
-            ))}
-          </div>
-        )}
+      <div className="p-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 grid-flow-row auto-rows-min gap-12">
+        {dummy_data.documents.map((document) => (
+          <Thumbnail data={document} />
+        ))}
       </div>
       <div className="more mx-auto rounded-lg my-4 align-center border-2 font-bold border-black px-10 py-2 w-fit hover:bg-dark-orange cursor-pointer">
         Load More
